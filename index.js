3e545c3c265e513173da81b601a75077c2ff4181
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2024-02-13 12:59:04 -0300

    JS and styling start

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 import init, { greet } from "./pkg/hello_wasm.js";
+
+
+
+
+// document.body.addEventListener("load", function (ev) {
+//     document.getElementsByClassName("word-size-button")
+// });
+
+
+
+
+
 init().then(() => {
     greet("WebAssembly");
 });
+
+
+function setWordSize(size) {
+    const letterInputs = document.getElementsByClassName("letter-input");
+    letterInputs.array.forEach(function (input, index) {
+        if (index <= size)
+            input.style.display = "block";
+        else input.style.display = "hidden";
+    });
+}
