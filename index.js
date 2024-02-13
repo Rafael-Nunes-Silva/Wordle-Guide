6916f18565fa398903a28a13f0f5b72d4f7dcfe3
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2024-02-13 10:05:55 -0300

    Web Assembly testing

---
+++ index.js
@@ -0,0 +1,4 @@
+import init, { greet } from "./pkg/hello_wasm.js";
+init().then(() => {
+    greet("WebAssembly");
+});
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,28 @@
+use std::fmt::format;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     pub fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub struct TestStruct {
+    xyz: i32,
+    abc: String,
+}
+#[wasm_bindgen]
+impl TestStruct {
+    pub fn new(x: i32, y: i32, z: i32, a: &str, b: &str, c: &str) -> TestStruct {
+        TestStruct {
+            xyz: x * y * z,
+            abc: format!("{}{}{}", a, b, c),
+        }
+    }
 }
